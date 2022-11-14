78e66f4bd8540f6bbb1456a139d7f79d0db34ef7
Fayzan Rauf <fizzy@Fayzans-MBP-2.local>
2022-11-14 00:29:40 -0500

    yeet

--- urls.py
+++ urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from . import views
 
 urlpatterns = [
     path('TapBox/project', include('TapBox.urls')),
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns = [path("", views.home, name="home"), path("status", name="status")]
--- views.py
+++ views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the TapBox index")
+    return HttpResponse("<h1>Welcome to the TapBox</h1> <h1>Battery Status: </h1> <h1>Reset</h1> <h1>Logs: </h1> Previous Unlocks")
+     
 
 def detail(request, question_id):
     return HttpResponse("The battery level is %s" % question_id)
